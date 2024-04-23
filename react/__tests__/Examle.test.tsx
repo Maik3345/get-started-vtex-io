59d9a0d0f6692dfb3f98f0fde2b9b8924ec635cd
Michael Stiven Restrepo Alvarez <michael.restrepo@puntoscolombia.com>
2024-04-22 23:39:03 -0500

    refactor: :arrow_up: update package.josn dependencies and fix vulnerabilities

--- react/__tests__/Examle.test.tsx
+++ react/__tests__/Examle.test.tsx
@@ -1,15 +1,10 @@
-/* eslint-env jest */
 import React from 'react'
 import { render } from '@vtex/test-tools/react'
 
 import Example from '../Example'
 
 test('should render the "Example component" text', () => {
-  const props = {
-    text: 'Example component',
-  }
-
-  const { getByText } = render(<Example {...props} />)
+  const { getByText } = render(<Example />)
 
   expect(getByText(/Example component/)).toBeDefined()
 })
